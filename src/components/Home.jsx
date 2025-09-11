2622c1f6a12869d88347c2a9a45c292f6369bc09
BENGALURU YASWANTH KUMAR <yaswanthkumar9391@gmail.com>
2025-09-11 06:14:29 +0530

    error Reesolve

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Loader from '../components/Loader'
-import Background from './background'
+
 const foodMenu = {
   Starters: {
     description: 'Enjoy our delicious starters to begin your meal.',
